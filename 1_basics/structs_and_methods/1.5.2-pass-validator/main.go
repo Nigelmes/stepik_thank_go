b50319003ec05c16f3d504c6e5e008d2c742f3b5
Nigelms <almazchic@gmail.com>
2023-03-17 00:44:19 +0300

    structs and methods

---
+++ 1_basics/structs_and_methods/1.5.2-pass-validator/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+	"unicode/utf8"
+)
+
+// validator проверяет строку на соответствие некоторому условию
+// и возвращает результат проверки
+type validator func(s string) bool
+
+// digits возвращает true, если s содержит хотя бы одну цифру
+// согласно unicode.IsDigit(), иначе false
+func digits(s string) bool {
+	//  var check int
+	for _, a := range s {
+		if unicode.IsDigit(a) {
+			return true
+		}
+	}
+	return false
+}
+
+// letters возвращает true, если s содержит хотя бы одну букву
+// согласно unicode.IsLetter(), иначе false
+func letters(s string) bool {
+	for _, a := range s {
+		if unicode.IsLetter(a) {
+			return true
+		}
+	}
+	return false
+}
+
+// minlen возвращает валидатор, который проверяет, что длина
+// строки согласно utf8.RuneCountInString() - не меньше указанной
+func minlen(length int) validator {
+	return func(s string) bool {
+		return utf8.RuneCountInString(s) >= length
+	}
+}
+
+// and возвращает валидатор, который проверяет, что все
+// переданные ему валидаторы вернули true
+func and(funcs ...validator) validator {
+	return func(s string) bool {
+		for _, a := range funcs {
+			if a(s) == false {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// or возвращает валидатор, который проверяет, что хотя бы один
+// переданный ему валидатор вернул true
+func or(funcs ...validator) validator {
+	return func(s string) bool {
+		for _, a := range funcs {
+			if a(s) == true {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// password содержит строку со значением пароля и валидатор
+type password struct {
+	value string
+	validator
+}
+
+// isValid() проверяет, что пароль корректный, согласно
+// заданному для пароля валидатору
+func (p *password) isValid() bool {
+	result := p.validator(p.value)
+	return result
+}
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+	validator := or(and(digits, letters), minlen(10))
+	p := password{s, validator}
+	fmt.Println(p.isValid())
+}
